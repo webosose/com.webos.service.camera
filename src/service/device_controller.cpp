1f8f37210bd6827c3a42adc52cf840286ce4078a
Sapna Kumari <sapna.kumari@lge.com>
2019-02-13 13:34:40 +0530

    Add optional input parameter to getProperties API
    :Release Notes:
    Add optional input parameter "params" in getProperties API
    
    :Detailed Notes
    -optional input parameter "params" added in getProperties
    to get only specific property requested by user
    -API JIRA ticket
    [API-3407] Camera service API's for multiple application
    access
    
    :Testing Performed:
    Local OSE build setup due to dependency on other components
    like PDM etc. and unit tested below test cases.
    [QO-3777] Verify Camera Record/Stop using LGE camera Device with
    Sharing ON
    [QO-3779] Verify Camera preview for longer period of time using
    LGE camera Device with Sharing OFF
    [QO-3784] Verify Camera capture using LGE camera Device with
    Sharing ON
    [QO-3785] Verify Camera Preview using LGE camera Device with
    Sharing ON
    [QO-3811] Verify Camera Capture Functionality during Record with
    Sharing ON
    [QO-3829] Verify Camera Preview using LGE camera Device with
    Sharing OFF
    [QO-3834] Verify Camera capture using LGE camera Device with
    Sharing OFF
    [QO-3851] Verify Camera Record/Stop using LGE camera Device with
    Sharing OFF
    [QO-3855] Verify Camera preview for longer period of time using
    LGE camera Device with Sharing ON
    [QO-3856] Verify Camera Capture Functionality during Record with
    Sharing OFF
    
    :QA Notes:
    NA
    
    :Issues Addressed:
    [PLAT-73777] Modify getProperties API in Camera Service
    
    Change-Id: I59601bfa710ae4c616885d229a0619c415e8443d
    Reviewed-on: http://gpro.lgsvl.com/230259
    Reviewed-by: OSE Helper Bot <ose@ci.lgsvl.net>
    Reviewed-by: Pankaj Maharana <pankaj.maharana@lge.com>
    Tested-by: Pankaj Maharana <pankaj.maharana@lge.com>

--- src/service/device_controller.cpp
+++ src/service/device_controller.cpp
@@ -45,9 +45,9 @@ DEVICE_RETURN_CODE_T DeviceControl::writeImageToFile(const void *p, int size) co
   if (path.empty())
     path = "/tmp/";
 
-  //check if specified location ends with '/'
+  // check if specified location ends with '/'
   char ch = path.back();
-  if('/' != ch)
+  if ('/' != ch)
     path += "/";
 
   // create file to save data based on format
@@ -442,8 +442,8 @@ DEVICE_RETURN_CODE_T DeviceControl::captureImage(void *handle, int ncount, CAMER
 {
   PMLOG_INFO(CONST_MODULE_DC, "captureImage started ncount : %d \n", ncount);
 
-  //update image locstion if there is a change
-  if(str_imagepath_ != imagepath)
+  // update image locstion if there is a change
+  if (str_imagepath_ != imagepath)
     str_imagepath_ = imagepath;
 
   // validate if saved format and capture image format are same or not
--- src/service/json_parser.h
+++ src/service/json_parser.h
@@ -23,6 +23,7 @@
 #include <iostream>
 #include <stdio.h>
 #include <string>
+#include <vector>
 
 class MethodReply
 {
@@ -326,6 +327,9 @@ public:
   }
   CAMERA_PROPERTIES_T rGetCameraProperties() const { return ro_camproperties_; }
 
+  void setParams(const std::string& param) { str_params_.push_back(param); }
+  std::vector<std::string> getParams() { return str_params_; }
+
   void setMethodReply(bool returnvalue, int errorcode, std::string errortext)
   {
     objreply_.setReturnValue(returnvalue);
@@ -342,6 +346,7 @@ public:
 private:
   int n_devicehandle_;
   CAMERA_PROPERTIES_T ro_camproperties_;
+  std::vector<std::string> str_params_;
   MethodReply objreply_;
 };
 
@@ -406,5 +411,7 @@ private:
 
 void createJsonStringFailure(MethodReply, jvalue_ref &);
 void createGetPropertiesJsonString(CAMERA_PROPERTIES_T *, void *, jvalue_ref &);
+void createGetPropertiesOutputParamJsonString(const std::string, CAMERA_PROPERTIES_T *,
+                                              jvalue_ref &);
 
 #endif /*SRC_SERVICE_JSON_PARSER_H_*/
