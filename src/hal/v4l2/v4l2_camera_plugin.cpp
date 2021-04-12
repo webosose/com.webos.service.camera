a477e2ec608dc5e3ff1bd796efeb0a839b4db18f
sungho <shl.lee@lge.com>
2021-04-12 15:57:55 +0900

    Copy the actual data size.
    :Release Notes:
    Copy the actual data size.
    
    :Detailed Notes:
    used bytesused(v4l2 parameter)
    
    :Testing Performed:
    build ok & API test
    but need more test
    
    :QA Notes:
    NA
    
    :Issues Addressed:
    [PLAT-139252] Issues on content size delivery in shared memory
    
    Change-Id: Ie5ca3560848b71e674ef6fb653cc724f645123fd

--- src/hal/v4l2/v4l2_camera_plugin.cpp
+++ src/hal/v4l2/v4l2_camera_plugin.cpp
@@ -205,7 +205,7 @@ int V4l2CameraPlugin::getBuffer(buffer_t *outbuf)
   {
   case IOMODE_MMAP:
   {
-    memcpy(outbuf->start, buffers_[buf.index].start, buf.length);
+    memcpy(outbuf->start, buffers_[buf.index].start, buf.bytesused);
     outbuf->length = buf.bytesused;
     outbuf->index = buf.index;
     break;
