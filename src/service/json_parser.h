960c18e7a742a60da0f28e87e98af60c317bdb3a
shl.lee <shl.lee@lge.com>
2021-10-08 17:38:36 +0900

    Fix insufficient string copy
    :Release Notes:
    Fix insufficient string copy
    
    :Detailed Notes:
    Fix insufficient string copy
    
    :Testing Performed:
    local build & test OK
    
    :QA Notes:
    NA
    
    :Issues Addressed:
    [TVPLAT-142005]
    
    Change-Id: I6dea02ae7099f2a26c54201252b659d5a3dd6cf8

--- src/service/json_parser.h
+++ src/service/json_parser.h
@@ -255,9 +255,9 @@ public:
 
   void setCameraInfo(const camera_device_info_t& r_ininfo)
   {
-    strncpy(ro_info_.str_devicename, r_ininfo.str_devicename, 32);
-    strncpy(ro_info_.str_vendorid, r_ininfo.str_vendorid, 32);
-    strncpy(ro_info_.str_productid, r_ininfo.str_productid, 32);
+    strncpy(ro_info_.str_devicename, r_ininfo.str_devicename, (CONST_MAX_STRING_LENGTH - 1));
+    strncpy(ro_info_.str_vendorid, r_ininfo.str_vendorid, (CONST_MAX_STRING_LENGTH - 1));
+    strncpy(ro_info_.str_productid, r_ininfo.str_productid, (CONST_MAX_STRING_LENGTH - 1));
     ro_info_.b_builtin = r_ininfo.b_builtin;
     ro_info_.n_codec = r_ininfo.n_codec;
     ro_info_.n_format = r_ininfo.n_format;
