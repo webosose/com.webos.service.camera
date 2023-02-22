f9df64781e5f6eaf8c93524b44366925a4cafc0e
Seoungil Kang <seoungil.kang@lge.com>
2023-02-22 18:50:16 +0900

    Imporve camera-plugin-inspect
    :Release Notes:
    Imporve camera-plugin-inspect
    
    :Detailed Notes:
    Imporve camera-plugin-inspect
    
    1. By default,it will scan $CAMERA_PLUGIN_PATH and make $CAMERA_REGISTRY_PATH
       file if there are nothing but argv[0] in commandline arguments.
    
    2. If user specify list of feature name, dumps information of these.
    
    3. If user set --read_registry, it will read registry file instead scan
       plugin dir for making registry data structure.
    
    4. If user set --verify_feature and specify list of feature name, it
       will load and test these.
    
    5. User can make combinations of options.
    
    :Testing Performed:
    Local Build & Test : OK
    
    :QA Notes:
    NA
    
    :Issues Addressed:
    [TVPLAT-265729]
    
    Change-Id: Ia64bc584fe4f0f5fe769d7490ab8c996950e5cea

--- tests/test-app/camera-plugin-inspect/camera_plugin_inspect.cpp
+++ tests/test-app/camera-plugin-inspect/camera_plugin_inspect.cpp
@@ -1,3 +1,4 @@
+#include <glib.h>
 #include <iostream>
 #include <plugin_factory.hpp>
 #include <plugin_interface.hpp>
@@ -9,7 +10,7 @@ public:
     virtual ~PluginScanner(void) {}
 
 public:
-    bool scan(void)
+    bool scanPluginPath(void)
     {
         bool ret = true;
 
@@ -61,6 +62,43 @@ public:
         return ret;
     }
 
+    std::string findPluginNameHaveFeature(const char *szFeatureName)
+    {
+        for (auto &p : lstPluginInfo_)
+        {
+            auto it = std::find_if(std::begin(p->lstFeatures_), std::end(p->lstFeatures_),
+                                   [&szFeatureName](const auto &f) { return szFeatureName == f; });
+            if (it != std::end(p->lstFeatures_))
+                return p->strName_;
+        }
+        return "";
+    }
+
+    std::string getCategory(const char *szFeatureName)
+    {
+        for (auto &p : lstPluginInfo_)
+        {
+            auto it = std::find_if(std::begin(p->lstFeatures_), std::end(p->lstFeatures_),
+                                   [&szFeatureName](const auto &f) { return szFeatureName == f; });
+            if (it != std::end(p->lstFeatures_))
+                return p->strCategory_;
+        }
+        return "";
+    }
+
+    std::vector<std::string> getFeatureList(void)
+    {
+        std::vector<std::string> vecFeatures;
+        for (auto &p : lstPluginInfo_)
+        {
+            for (auto &f : p->lstFeatures_)
+            {
+                vecFeatures.emplace_back(f);
+            }
+        }
+        return vecFeatures;
+    }
+
     void printPluginInfo(std::string &strPluginName)
     {
         auto it =
@@ -106,21 +144,140 @@ public:
     bool dumpJsonFile(void)
     {
         bool ret = false;
+        std::cout << "Check : " << g_path_get_dirname(getRegistryFilePath()) << std::endl;
+        if (!g_file_test(g_path_get_dirname(getRegistryFilePath()), G_FILE_TEST_IS_DIR))
+        {
+            std::cout << "Make Path : " << g_path_get_dirname(getRegistryFilePath()) << std::endl;
+            g_mkdir_with_parents(g_path_get_dirname(getRegistryFilePath()), 0755);
+        }
+
         std::ofstream ofs;
         ofs.open(getRegistryFilePath(), std::ios_base::out);
         json j = lstPluginInfo_;
         ofs << std::setw(4) << j << std::endl;
+        ofs.flush();
         ofs.close();
+        ret = true;
+
         return ret;
     }
 };
 
+struct OptionParser
+{
+    gboolean bScanPluginPath_{TRUE};
+    gboolean bReadRegistry_{FALSE};
+    gboolean bVerifyFeature_{FALSE};
+    gchar **vszFeatureNames_{NULL};
+    guint nFeatureNames_{0};
+
+public:
+    OptionParser(void) {}
+    ~OptionParser(void) {}
+
+public:
+    void parse(int &argc, char **&argv)
+    {
+        GError *err            = NULL;
+        GOptionEntry options[] = {
+            {"read_registry", 0, 0, G_OPTION_ARG_NONE, &bReadRegistry_,
+             "Read informations from camera plugin registry file.", NULL},
+            {"verify_feature", 0, 0, G_OPTION_ARG_NONE, &bVerifyFeature_,
+             "Verify features are running properly.", NULL},
+            {G_OPTION_REMAINING, 0, 0, G_OPTION_ARG_FILENAME_ARRAY, &vszFeatureNames_, NULL},
+            {NULL}};
+
+        g_set_prgname("camera-plugin-inspect");
+
+        auto ctx = g_option_context_new("[feature name]");
+        g_option_context_add_main_entries(ctx, options, NULL);
+        if (!g_option_context_parse(ctx, &argc, &argv, &err))
+        {
+            g_print("Error initializing: %s\n", err->message ? err->message : "(NULL)");
+            g_clear_error(&err);
+            g_option_context_free(ctx);
+            return;
+        }
+        g_option_context_free(ctx);
+
+        if (vszFeatureNames_ != nullptr)
+            nFeatureNames_ = g_strv_length(vszFeatureNames_);
+
+        if (bReadRegistry_ == TRUE)
+            bScanPluginPath_ = FALSE;
+    }
+};
+
+void verifyFeature(PluginFactory &fact, const char *szFeatureName)
+{
+    std::cout << "====" << std::endl;
+
+    auto pFeature    = fact.createFeature(szFeatureName);
+    void *pInterface = nullptr;
+
+    if (pFeature == nullptr)
+    {
+        std::cout << "Feature " << szFeatureName << " is not available." << std::endl;
+        return;
+    }
+
+    std::cout << "[TEST #1] Interface Query for " << szFeatureName;
+    pFeature->queryInterface(szFeatureName, &pInterface);
+    if (pInterface != nullptr)
+        std::cout << " - OK" << std::endl;
+    else
+        std::cout << " - FAIL" << std::endl;
+
+    std::cout << "[TEST #2] Unload Feature for " << szFeatureName << std::endl;
+}
+
 int main(int argc, char *argv[])
 {
+    OptionParser opt;
+    opt.parse(argc, argv);
+
     PluginScanner scan;
-    scan.scan();
-    scan.dump();
-    scan.dumpJson();
-    scan.dumpJsonFile();
+
+    if (opt.bScanPluginPath_)
+    {
+        scan.scanPluginPath();
+        scan.dumpJsonFile();
+    }
+    else
+    {
+        scan.readPluginRegistry();
+    }
+
+    if (opt.nFeatureNames_ == 0)
+    {
+        if (!opt.bVerifyFeature_)
+        {
+            scan.dump();
+            return 0;
+        }
+
+        std::vector<std::string> vecFeatures = scan.getFeatureList();
+
+        PluginFactory fact;
+        for (auto &feature : vecFeatures)
+            verifyFeature(fact, feature.c_str());
+        return 0;
+    }
+
+    if (opt.bVerifyFeature_)
+    {
+        PluginFactory fact;
+        for (char **f = opt.vszFeatureNames_; *f != NULL; f++)
+            verifyFeature(fact, *f);
+
+        return 0;
+    }
+
+    for (char **f = opt.vszFeatureNames_; *f != NULL; f++)
+    {
+        std::string strPluginName = scan.findPluginNameHaveFeature(*f);
+        scan.printPluginInfo(strPluginName);
+    }
+
     return 0;
 }
